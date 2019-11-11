9c4ce8116106bc09ff20f2849e2e90af22810861
will <wpringle@uoguelph.ca>
2019-11-11 15:23:36 -0500

    created code that uses the correct data

--- a4.c
+++ a4.c
@@ -60,25 +60,22 @@ void dater(char* str, int *month, int *day, int *year) {
 	int size = strlen(str);
 	int i = 0;
 	int j = 0;
-	
+
 	for(i = 0; i < size; i++) {
 		
-		
-		
-		if(!numSlash) {
-			*month = multi * str[i];
+		if(isdigit(str[i]) && !numSlash) {
+			*month = *month + multi * (str[i] - '0');
 			multi = multi / 10;
 			
-		} else if (numSlash == 1) {
-			*day = multi * str[i];
+		} else if (isdigit(str[i]) && numSlash == 1) {
+			*day = *day + multi * (str[i] - '0');
 			multi = multi / 10;
 			
-		} else if (numSlash == 2) {
-			*year = multi * str[i];
+		} else if (isdigit(str[i]) && numSlash == 2) {
+			*year = *year + multi * (str[i] - '0');
 			multi = multi / 10;
 			
 		}
-		
 		
 		if(str[i] == '/') {
 			numSlash++;
@@ -90,11 +87,9 @@ void dater(char* str, int *month, int *day, int *year) {
 			
 		}
 		
-		
-		
 	}
 	
-	
+/*	printf("%d\t%d\t%d\t\t\t%s\n", *month, *day, *year, str);*/
 }
 
 int hash1(char* str, int max) {
@@ -122,27 +117,89 @@ int hash1(char* str, int max) {
 	j = k >> j;
 	
 	new = new ^ j;
+
+	if(new > max) {
+		
+		new = new % max;
+		
+	}
+
+    return (int)new;
+}
+
+int hash2(char* str, int max) {
+	int size = strlen(str);
+	int i = 0;
+	int flag = 0;
+	int new = hash1(str, max); /* the new to return */
+	int multi = 1000000;
 	
+	for(i = 0; i < size; i++) {
+		
+		if(!flag && (str[i] == '-' || isdigit(str[i]))) {
+			flag = 1;
+		}
+		
+		if(flag) {
+			new = new + str[i] * multi;
+			multi = multi / 10;
+			new = new + str[i] * multi;
+		}
+	}
+	
+	if(new > max) {
+		new = new - 2000000;
+		
+		if(new > max) {
+			new = new % max;
+		}
+		
+		else if (new < 0) {
+			
+			new = hash1(str, max);
+			puts("hello");
+			
+		}
+	}
+	
+	return new;
+}
+
+int hash3(char* str, int max) {
+	int new = 0;
+	int month = 0;
+	int day = 0;
+	int year = 0;
+	int temp = 0;
+	dater(str, &month, &day, &year);
+/*	printf("\nday = %d\tmonth = %d\t year = %d\n%s\n\n", day, month, year, str);*/
+	
+	/* if the value isn't in the correct format */
+	if((!month || day) && !year) {
+		return hash1(str, max);
+	}
+	
+	/* add the number of days */
+	new = month * 31 + day;
+	
+	if(year >= 2000){
+		new = new * 10 + 2;
+		
+	}
+	else {
+		new = new * 10 + 9;
+		
+	}
+	
+	temp = year / 100;
+	year = year - temp;
+	new = new + year * 10000;
 	
 	if(new > max) {
 		
 		new = new % max;
 		
 	}
-		
-    return (int)new;
-}
 
-int hash2(char* str, int max) {
-	int new = 0;
-	int num = numify(str, max);
-	
-	new = num;
-	
-	
 	return new;
-}
-
-int hash3(char* str, int max) {
-	return numify(str, max);
 }
