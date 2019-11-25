674521971ee0a6e8c9704b6d9898920e9b1161fc
Will <wpringle@uoguelph.ca>
2019-11-25 08:15:13 -0500

    Made the chamberView class into a popup to display the map as a popup

--- src/gui/ChamberView.java
+++ src/gui/ChamberView.java
@@ -5,30 +5,21 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Popup;
 
 public class ChamberView extends GridPane {
     private String map;
     private int length;
     private int width;
-
+	private Popup popup;
 
     public ChamberView(int len, int wid){
-       map = "/res/DungeonLayoutTemplate.png";
-       length = len;
-       width = wid;
-
+		map = "/res/DungeonLayoutTemplate.png";
+		length = len;
+		width = wid;
+		popup = new Popup();
+		
+		
     }
-
-
-    public Node floorFactory(String img) {
-        Image map = new Image(getClass().getResourceAsStream(img));
-        Label toReturn = new Label();
-        ImageView imageView = new ImageView(map);
-        imageView.setFitWidth(50);
-        imageView.setFitHeight(50);
-        toReturn.setGraphic(imageView);
-        return toReturn;
-    }
-
 
 }
